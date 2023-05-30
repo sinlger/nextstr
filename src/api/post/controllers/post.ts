2b8ce16eb6b2c3825a1e5404818cc087fea9ad91
sinlger <donghe587@139.com>
2023-05-30 23:17:36 +0800

    修改post 返回格式

---
+++ src/api/post/controllers/post.ts
@@ -0,0 +1,28 @@
+/**
+ * post controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::post.post', ({ strapi }) => ({
+  async exampleAction(ctx) {
+    try {
+      ctx.body = 'ok';
+    } catch (err) {
+      ctx.body = err;
+    }
+  },
+  async find(ctx) {
+    // some custom logic here
+    ctx.query = { ...ctx.query, }
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+    const attrs = data.map(v => {
+      return {
+        id: v.id,
+        ...v.attributes
+      }
+    })
+    return { data: attrs, meta };
+  },
+}));
